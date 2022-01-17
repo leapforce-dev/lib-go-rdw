3bc6a0aff84d421bfd9ce0b86702d6e7fd5aa308
caselongo <caselongo@gmail.com>
2022-01-17 16:48:50 +0100

    added APIReset

--- Service.go
+++ Service.go
@@ -74,3 +74,7 @@ func (service Service) APIKey() string {
 func (service Service) APICallCount() int64 {
 	return service.httpService.RequestCount()
 }
+
+func (service *Service) APIReset() {
+	service.httpService.ResetRequestCount()
+}
